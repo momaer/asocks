485e7f29c2a91144e2dd78b98832098df8d11851
gaoshijie <gaoshijie@pwrd.com>
2014-10-23 20:31:48 +0800

    init

---
+++ local2.c
@@ -0,0 +1,344 @@
+/*
+ * local.c
+ *
+ *  Created on: Oct 15, 2014
+ *      Author: gaoshijie
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <pthread.h>
+
+static int listen_port = 1080;
+static char* server = NULL;
+static int server_port = 10800;
+static struct addrinfo *result;
+
+static unsigned char* xorencode(void *data, int len)
+{
+	unsigned char *buf = (unsigned char *)data;
+	while(len-- > 0)
+	{
+		*(buf+len) ^= 0xff;
+	}
+
+	return buf;
+}
+
+static int send_all(int fd, void *data, int len)
+{
+
+	return len;
+}
+
+static int recv_all(int fd, void *data, int len)
+{
+	char *buf = (char *)data;
+	int l = 0;
+	while(l < len)
+	{
+		int s = recv(fd, buf+l, len-l, 0);
+
+		if(s <= 0)
+			return s;
+		else
+			l += s;
+	}
+
+	return l;
+}
+
+static void redirect_data(int fd, int remotefd)
+{
+	fd_set read;
+
+	while(1)
+	{
+		FD_ZERO(&read);
+		FD_SET(fd, &read);
+		FD_SET(remotefd, &read);
+
+		int ret = select(fd+remotefd, &read, NULL, NULL, NULL);
+		if(ret == -1)
+			continue;
+
+		char buf[4096] = {0};
+
+		if(FD_ISSET(fd, &read))
+		{
+			int len = recv(fd, buf, 4096, 0);
+
+			if(len == -1 || len == 0)
+			{
+				shutdown(fd, SHUT_RDWR);
+				close(fd);
+				shutdown(remotefd, SHUT_RDWR);
+				close(remotefd);
+				return;
+			}
+			send(remotefd, xorencode(buf,len), len, 0);
+		}
+
+		if(FD_ISSET(remotefd, &read))
+		{
+			int len = recv(remotefd, buf, 4096, 0);
+
+			if(len == -1 || len == 0)
+			{
+				shutdown(fd, SHUT_RDWR);
+				close(fd);
+				shutdown(remotefd, SHUT_RDWR);
+				close(remotefd);
+				return;
+			}
+			send(fd, xorencode(buf,len), len, 0);
+		}
+	}
+}
+
+static void *worker(void *arg)
+{
+	int clientfd = *((int *)arg);
+
+	char buf[262] = {0};
+	int ret = recv(clientfd, buf, 262, 0);
+	if(ret <=0)
+	{
+		shutdown(clientfd, SHUT_RDWR);
+		close(clientfd);
+	}
+
+	buf[0] = 0x05;
+	buf[1] = 0x00;
+	send(clientfd, buf, 2, 0);
+
+	char sendbuf[256] = {0};
+	char sendbufindex = 0;
+
+	bzero(buf, 262);
+//	recv(clientfd, buf, 4, 0);
+	ret = recv_all(clientfd, buf, 4);
+	if(ret <= 0)
+	{
+		printf("recv first 4 bytes request.%d\n", ret);
+		shutdown(clientfd, SHUT_RDWR);
+		close(clientfd);
+	}
+
+	/* only accept connect cmd  */
+	if(buf[1] != 1)
+	{
+		printf("only accept connect cmd. recv cmd:[%d].\n", buf[1]);
+		shutdown(clientfd, SHUT_RDWR);
+		close(clientfd);
+		return 0;
+	}
+
+	memcpy(sendbuf+sendbufindex, buf+3, 1);
+	sendbufindex += 1;
+
+	/* ip v4 */
+	if(buf[3] == 1)
+	{
+		char remoteip[4] = {0};
+//		recv(clientfd, remoteip, 4, 0);
+		ret = recv_all(clientfd, remoteip, 4);
+		if(ret <= 0)
+		{
+			shutdown(clientfd, SHUT_RDWR);
+			close(clientfd);
+		}
+
+		memcpy(sendbuf+sendbufindex, remoteip, 4);
+		sendbufindex += 4;
+	}
+	/* domain name */
+	else if(buf[3] == 3)
+	{
+		char len[1] = {0};
+		ret = recv(clientfd, len, 1, 0);
+		if(ret <= 0)
+		{
+			shutdown(clientfd, SHUT_RDWR);
+			close(clientfd);
+		}
+
+		char *domainame = (char *)malloc(len[0]);
+//		recv(clientfd, domainame, len[0], 0);
+		recv_all(clientfd, domainame, len[0]);
+		if(ret <= 0)
+		{
+			shutdown(clientfd, SHUT_RDWR);
+			close(clientfd);
+		}
+
+		memcpy(sendbuf+sendbufindex, len, 1);
+		sendbufindex += 1;
+		memcpy(sendbuf+sendbufindex, domainame, len[0]);
+		sendbufindex += len[0];
+
+		free(domainame);
+	}
+	/* ip v6 */
+	else if(buf[3] == 4)
+	{
+		printf("remote addr type ip v6 is not supported.\n");
+		shutdown(clientfd, SHUT_RDWR);
+		close(clientfd);
+		return 0;
+	}
+	else
+	{
+		//shutdown and destroy client
+		printf("remote addr type error.\n");
+		shutdown(clientfd, SHUT_RDWR);
+		close(clientfd);
+		return 0;
+	}
+
+	char dstportbuf[2] = {0};
+//	recv(clientfd, dstportbuf, 2, 0);
+	ret = recv_all(clientfd, dstportbuf, 2);
+	if(ret <= 0)
+	{
+		shutdown(clientfd, SHUT_RDWR);
+		close(clientfd);
+	}
+
+	//printf("remote port:%d\n", ntohs( *((unsigned short int *)dstportbuf) ) );
+	memcpy(sendbuf+sendbufindex, dstportbuf, 2);
+	sendbufindex += 2;
+
+	char response[4] = {0x05, 0x00, 0x00, 0x01};
+	send(clientfd, response, 4, 0);
+
+	char bndaddr[4] = {0};
+	inet_pton(AF_INET, "0.0.0.0", bndaddr);
+	send(clientfd, bndaddr, 4, 0);
+
+	unsigned short int bndport = htons((short)listen_port);
+	send(clientfd, &bndport, 2, 0);
+
+	/* connect to server */
+	int remotefd = socket(AF_INET, SOCK_STREAM, 0);
+	int conn = connect(remotefd, result->ai_addr, result->ai_addrlen);
+
+	if(conn == 0)
+	{
+		char username[10] = {0};
+		strcpy(username, "gaoshijie");
+		char password[9] = {0};
+		strcpy(password, "password");
+
+		char ulen = strlen(username);
+		char plen = strlen(password);
+
+		send(remotefd, xorencode(&ulen,1), 1, 0);
+		xorencode(&ulen,1);
+		send(remotefd, xorencode(username, ulen), ulen, 0);
+
+		send(remotefd, xorencode(&plen,1), 1, 0);
+		xorencode(&plen,1);
+		send(remotefd, xorencode(password,plen), plen, 0);
+
+		send(remotefd, xorencode(sendbuf,sendbufindex), sendbufindex, 0);
+	}
+	else
+	{
+		printf("connect to server failed. return value:%d\n", conn);
+		shutdown(clientfd, SHUT_RDWR);
+		close(clientfd);
+		shutdown(remotefd, SHUT_RDWR);
+		close(remotefd);
+		return 0;
+	}
+
+	redirect_data(clientfd, remotefd);
+	return 0;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+	if(argc < 4)
+	{
+		printf("Usage:%s listen_port server server_port\n", argv[0]);
+		return 1;
+	}
+
+	listen_port = atoi(argv[1]);
+	server = argv[2];
+	server_port = atoi(argv[3]);
+
+	signal(SIGPIPE, SIG_IGN);
+	signal(SIGABRT, SIG_IGN);
+
+	char p[5] = {0};
+	sprintf(p, "%d", server_port);
+
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	int err = getaddrinfo(server, p, &hints, &result);
+
+	if(err)
+	{
+		printf("getaddrinfo error.\n");
+		return 1;
+	}
+
+	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	assert(listenfd);
+
+	int opt = 1;
+	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+	struct sockaddr_in addr;
+	bzero(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	inet_pton(AF_INET, "0.0.0.0", &addr.sin_addr);
+	addr.sin_port = htons(listen_port);
+
+	int ret = bind(listenfd, (struct sockaddr *)&addr, sizeof(addr));
+	assert(ret != -1);
+
+	ret = listen(listenfd, 32);
+	assert(ret == 0);
+
+	printf("listening...\n");
+
+	while(1)
+	{
+		int clientfd = accept(listenfd, NULL, NULL);
+//		printf("new connection.\n");
+
+		pthread_t threadid;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+		int ret = pthread_create(&threadid, &attr, &worker, &clientfd);
+		pthread_attr_destroy(&attr);
+
+		/* EAGAIN? */
+		if(ret != 0)
+		{
+			printf("pthread_create return %d.\n", ret);
+			close(clientfd);
+		}
+	}
+
+	freeaddrinfo(result);
+	close(listenfd);
+	return 0;
+}
---
+++ server2.c
@@ -0,0 +1,292 @@
+/*
+ * server.c
+ *
+ *  Created on: Oct 22, 2014
+ *      Author: gaoshijie
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+
+struct client
+{
+	int fd;
+	char state;
+	int remotefd;
+};
+
+static unsigned char* xorencode(void *data, int len)
+{
+	unsigned char *buf = (unsigned char *)data;
+	while(len-- > 0)
+	{
+		*(buf+len) ^= 0xff;
+	}
+
+	return buf;
+}
+
+void close_socket(int fd)
+{
+	shutdown(fd, SHUT_RDWR);
+	close(fd);
+}
+
+void redirect_data(int fd, int remotefd)
+{
+	fd_set read;
+
+	while(1)
+	{
+		FD_ZERO(&read);
+		FD_SET(fd, &read);
+		FD_SET(remotefd, &read);
+
+		int ret = select(fd+remotefd, &read, NULL, NULL, NULL);
+
+		if(ret == -1)
+		{
+			continue;
+		}
+
+		char buf[4096] = {0};
+
+		if(FD_ISSET(fd, &read))
+		{
+			int len = recv(fd, buf, 4096, 0);
+
+			if(len == -1 || len == 0)
+			{
+				close_socket(fd);
+				close_socket(remotefd);
+				return;
+			}
+			send(remotefd, xorencode(buf,len), len, 0);
+		}
+
+		if(FD_ISSET(remotefd, &read))
+		{
+			int len = recv(remotefd, buf, 4096, 0);
+
+			if(len == -1 || len == 0)
+			{
+				close_socket(fd);
+				close_socket(remotefd);
+				return;
+			}
+			send(fd, xorencode(buf,len), len, 0);
+		}
+	}
+}
+
+void *worker(void *arg)
+{
+	struct client *c = (struct client *)arg;
+
+	char buf[256] = {0};
+	int len = recv(c->fd, buf, 1, 0);
+	xorencode(buf, 1);
+
+	char* username = (char *)malloc(buf[0] + 1);
+	recv(c->fd, username, buf[0], 0);
+	xorencode(username, buf[0]);
+	*(username+buf[0]) = 0;
+
+	len = recv(c->fd, buf, 1, 0);
+	xorencode(buf, 1);
+	char* password = (char *)malloc(buf[0] + 1);
+	recv(c->fd, password, buf[0], 0);
+	xorencode(password, buf[0]);
+	*(password+buf[0]) = 0;
+
+//	printf("username:[%s], password:[%s]\n", username, password);
+	free(username);
+	free(password);
+	/* todo validate username and password */
+
+	len = recv(c->fd, buf, 1, 0);
+	xorencode(buf, 1);
+	char type = buf[0];
+
+	int remotefd = 0;
+
+	/* ip v4 */
+	if(type == 1)
+	{
+		char dstip[4] = {0};
+		len = recv(c->fd, dstip, 4, 0);
+		xorencode(dstip, 4);
+
+		unsigned int *pip = NULL;
+		pip = (unsigned int *)dstip;
+
+		char port[2] = {0};
+		len = recv(c->fd, port, 2, 0);
+		xorencode(port, 2);
+
+		unsigned short int *pport = NULL;
+		pport = (unsigned short int *)port;
+
+		remotefd = socket(AF_INET, SOCK_STREAM, 0);
+
+		struct sockaddr_in addr;
+		bzero(&addr, sizeof(addr));
+
+		addr.sin_family = AF_INET;
+		addr.sin_port = *pport;
+		addr.sin_addr.s_addr = *pip;
+
+		char humanaddr[256] = {0};
+		inet_ntop(AF_INET, (void *)&addr.sin_addr, humanaddr, 256);
+
+		int conn = connect(remotefd, (struct sockaddr *)&addr, sizeof(addr));
+		if(conn != 0)
+		{
+			printf("connecte to remote failed.host:[%s], port:[%d]\n", humanaddr, ntohs(addr.sin_port));
+			close_socket(c->fd);
+			close(remotefd);
+			remotefd = 0;
+		}
+		else
+			printf("connecte to remote success.host:[%s], port:[%d]\n", humanaddr, ntohs(addr.sin_port));
+	}
+	else if(type == 3)
+	{
+		recv(c->fd, buf, 1, 0);
+		xorencode(buf, 1);
+		char domainnamelen = buf[0];
+
+		char* domainname = (char *)malloc(domainnamelen + 1);
+		recv(c->fd, domainname, domainnamelen, 0);
+		xorencode(domainname, domainnamelen);
+		*(domainname + domainnamelen) = 0;
+
+		char port[3] = {0};
+		recv(c->fd, port, 2, 0);
+		xorencode(port, 2);
+
+		char temp = port[1];
+		port[1] = port[0];
+		port[0] = temp;
+
+
+		short int shortport = *((unsigned short int *)port);
+		char a[6] = {0};
+		sprintf(a, "%d", shortport);
+
+		//printf("remote domainname:[%s]remote port:[%s]\n", domainname, a);
+
+		struct addrinfo *result;
+
+		struct addrinfo hints;
+		memset(&hints, 0, sizeof(hints));
+//		hints.ai_family = AF_UNSPEC;
+		hints.ai_family = AF_INET;
+		hints.ai_socktype = SOCK_STREAM;
+
+		int err = getaddrinfo(domainname, a, &hints, &result);
+
+		if(err)
+		{
+			perror("getaddrinfo error.\n");
+			close_socket(c->fd);
+			close(remotefd);
+			remotefd = 0;
+		}
+		else
+		{
+			remotefd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+			int conn = connect(remotefd, result->ai_addr, result->ai_addrlen);
+
+			if(conn != 0)
+			{
+				printf("connect to remote failed.host:[%s], port:[%s]\n", domainname, a);
+				close_socket(c->fd);
+				close(remotefd);
+				remotefd = 0;
+			}
+			else
+				printf("connecte to remote success.host:[%s], port:[%s]\n", domainname, a);
+
+			freeaddrinfo(result);
+		}
+
+		free(domainname);
+	}
+
+	redirect_data(c->fd, remotefd);
+
+	free(c);
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	if(argc < 3)
+	{
+		printf("Usage:%s ip port\n", argv[0]);
+		return 1;
+	}
+
+	signal(SIGPIPE, SIG_IGN);
+	signal(SIGABRT, SIG_IGN);
+
+	char* ip = argv[1];
+	int port = atoi(argv[2]);
+
+	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	int opt = 1;
+	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+	struct sockaddr_in addr;
+	bzero(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	inet_pton(AF_INET, ip, &addr.sin_addr);
+	addr.sin_port = htons(port);
+
+	int ret = bind(listenfd, (struct sockaddr *)&addr, sizeof(addr));
+	assert(ret != -1);
+
+	ret = listen(listenfd, 32);
+	assert(ret == 0);
+
+	printf("listening ... \n");
+
+	while(1)
+	{
+		int clientfd = accept(listenfd, NULL, NULL);
+//		printf("new connection.\n");
+
+		struct client *new_client = (struct client *)malloc(sizeof(struct client));
+		new_client->fd = clientfd;
+		new_client->remotefd = 0;
+		new_client->state = 0;
+
+		pthread_t thread;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+		int ret = pthread_create(&thread, &attr, &worker, new_client);
+		pthread_attr_destroy(&attr);
+
+		if(ret != 0)
+		{
+			printf("pthread_create return %d.\n", ret);
+			close(clientfd);
+			free(new_client);
+		}
+	}
+
+	close(listenfd);
+	return 0;
+}
